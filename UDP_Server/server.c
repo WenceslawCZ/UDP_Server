30f520c143ecc461c82e7286cdfed5fbd417c01b
milmitt <58573295+milmitt@users.noreply.github.com>
2019-12-06 00:20:15 +0100

    Add files via upload

--- UDP_Server/server.c
+++ UDP_Server/server.c
@@ -10,7 +10,7 @@
 #include <math.h>
 #pragma warning(disable:4996) 
 
-unsigned int curr_seq_ID = 0;
+//unsigned int curr_seq_ID = 0;
 U_CHAR server_ID = 255;
 
 unsigned int get_info_server(unsigned int size) {
@@ -22,13 +22,14 @@ unsigned int get_info_server(unsigned int size) {
 	return val;
 }
 
-void mirror_buffer(U_CHAR* buf, size_t len_buf) {
-	char tmp_buf[4];
+void mirror_buffer(U_CHAR* buf, unsigned int len_buf) {
+	U_CHAR* tmp_buf = calloc(len_buf, sizeof(U_CHAR));
 	memcpy(tmp_buf, buf, len_buf);
 
 	for (int i = 0; i < len_buf; ++i) {
 		buf[i] = tmp_buf[(len_buf - 1) - i];
 	}
+	free(tmp_buf);
 }
 
 void get_dec_in_256base(unsigned int num, U_CHAR* buf, unsigned int len_buf) {
@@ -53,7 +54,7 @@ unsigned int get_256base_in_dec(U_CHAR* buf, unsigned int len_buf) {
 // convert ID from int to base 256
 void get_ID(U_CHAR* buf) {
 	memset(buf, 0, ID_LEN);
-	int num = curr_seq_ID++, i = 0;
+	int num = 0, i = 0;
 	while (num) {
 		buf[i++] = num % 256;
 		num = num / 256;
@@ -73,7 +74,7 @@ void get_CRC(U_CHAR* buf, size_t len_buf, U_CHAR* ret_buf) {
 // len_buf is here the whole length of the UDP data packet, so even ID and CRC
 bool check_CRC(U_CHAR* buf, size_t len_buf) {
 	bool ret = false;
-	uint64_t crc_val = crc_64_ecma(buf, len_buf - CRC_LEN);
+	uint64_t crc_val = crc_64_ecma(buf, len_buf);
 	if (!crc_val) {
 		ret = true;
 	}
@@ -83,7 +84,7 @@ bool check_CRC(U_CHAR* buf, size_t len_buf) {
 void append_comps(U_CHAR* buf, size_t len_buf) {
 	// append ID of the current packet sent
 	U_CHAR ID_buf[ID_LEN];
-	get_dec_in_256base(curr_seq_ID++, ID_buf, ID_LEN);
+	get_dec_in_256base(0, ID_buf, ID_LEN);
 	memcpy(&buf[len_buf], ID_buf, ID_LEN);
 
 	// append ID of the client
@@ -132,28 +133,37 @@ int main() {
 
 	client_info.data.sin_family = AF_INET;
 	client_info.data.sin_port = htons(CLIENT_DATA_PORT);
-	inet_pton(AF_INET, "192.168.43.208", &client_info.data.sin_addr);
+	inet_pton(AF_INET, "127.0.0.1", &client_info.data.sin_addr);
 	printf("Address family: %d\nIP address of server: %d\nServer port number: %d\n", client_info.data.sin_family, htonl(client_info.data.sin_addr.s_addr), htons(client_info.data.sin_port));
 
 	client_info.ack.sin_family = AF_INET;
 	client_info.ack.sin_port = htons(CLIENT_ACK_PORT);
-	inet_pton(AF_INET, "192.168.43.208", &client_info.ack.sin_addr);
+	inet_pton(AF_INET, "127.0.0.1", &client_info.ack.sin_addr);
 	printf("Address family: %d\nIP address of server: %d\nServer port number: %d\n", client_info.ack.sin_family, htonl(client_info.ack.sin_addr.s_addr), htons(client_info.ack.sin_port));
 
 	server_info.data.sin_family = AF_INET;
 	server_info.data.sin_port = htons(SERVER_DATA_PORT);
-	inet_pton(AF_INET, "192.168.43.208", &server_info.data.sin_addr);
+	inet_pton(AF_INET, "127.0.0.1", &server_info.data.sin_addr);
 	printf("Address family: %d\nIP address of server: %d\nServer port number: %d\n", server_info.data.sin_family, htonl(server_info.data.sin_addr.s_addr), htons(server_info.data.sin_port));
 
 	server_info.ack.sin_family = AF_INET;
 	server_info.ack.sin_port = htons(SERVER_ACK_PORT);
-	inet_pton(AF_INET, "192.168.43.208", &server_info.ack.sin_addr);
+	inet_pton(AF_INET, "127.0.0.1", &server_info.ack.sin_addr);
 	printf("Address family: %d\nIP address of server: %d\nServer port number: %d\n", server_info.ack.sin_family, htonl(server_info.ack.sin_addr.s_addr), htons(server_info.ack.sin_port));
 
+	// Bind sockets
+	int err1 = bind(server_sock.data, &server_info.data, sizeof(SOCKADDR_IN));
+	int err2 = bind(server_sock.ack, &server_info.ack, sizeof(SOCKADDR_IN));
+	if ((err1 == SOCKET_ERROR) || (err2 == SOCKET_ERROR)) {
+		int iSocketError = WSAGetLastError();
+		printf("WSALastError: %d\n", iSocketError);
+		WSACleanup();
+		return 100;
+	}
 
 	// Define buffer and etc.
 	U_CHAR Buffer_Name[DATA_LEN] = "";
-	U_CHAR Buffer_Path[PATH_REC_LEN + DATA_LEN] = "C:\\KDS\\KDS\\Rec_files";
+	U_CHAR Buffer_Path[PATH_REC_LEN + DATA_LEN] = "C:\\KDS\\KDS\\Rec_files\\";
 	U_CHAR Buffer_Rec[DATA_LEN + ID_LEN + IDS_LEN + CRC_LEN] = "";
 	U_CHAR Buffer_Send[40] = "";
 	int ID_from_packet = 0;
@@ -172,16 +182,15 @@ int main() {
 	unsigned int sizeof_file_dec[NUM_OF_CLIENTS] = { 0 };
 	unsigned int hash_ID[NUM_OF_CLIENTS] = { 0 };
 	unsigned int last_curr_recv_seq_ID[NUM_OF_CLIENTS] = { 0 };
-	unsigned int idx[NUM_OF_CLIENTS] = {0};
 	bool already_came = false;
-	U_CHAR file_data[NUM_OF_CLIENTS] = "";
+	U_CHAR *file_data[NUM_OF_CLIENTS];
 
 	fd_set rset;
 	FD_ZERO(&rset);
 	FD_SET(server_sock.data, &rset);
 	int nready, num_rec_Packets;
 	struct timeval timeout;
-	timeout.tv_sec = 5;
+	timeout.tv_sec = 2;
 	timeout.tv_usec = 0;
 	int num_of_packets = 0;
 	FILE *wptr;
@@ -217,29 +226,39 @@ int main() {
 
 				if(curr_recv_seq_ID == 0 && !already_came){
 					memcpy(Buffer_Name, Buffer_Rec, curr_recv_seq_ID_idx);
+					printf("%s\n", Buffer_Name);
+					already_came = true;
+
 				}else if (curr_recv_seq_ID == 1 && !already_came) {
 					sizeof_file_dec[curr_client_ID] = get_256base_in_dec(Buffer_Rec, SIZE_LEN);
 					file_data[curr_client_ID] = calloc(sizeof_file_dec[curr_client_ID], sizeof(U_CHAR));
 					hash_ID[curr_client_ID] = get_info_server(sizeof_file_dec[curr_client_ID]) + 2;
+					already_came = true;
+
 				}else if (curr_recv_seq_ID == hash_ID[curr_client_ID] && !already_came) {
 					// calc hash
 					SHA512_Update(&ctx, file_data[curr_client_ID], sizeof_file_dec[curr_client_ID]);
 					SHA512_Final(digest, &ctx);
 
 					if (!memcmp(digest, Buffer_Rec, SHA512_DIGEST_LENGTH)) {	//hash is the same
-						wptr = fopen(Buffer_Path, "rb");
+						memcpy(&Buffer_Path[PATH_REC_LEN], Buffer_Name, strlen(Buffer_Name));
+						printf("%s\n", Buffer_Path);
+						wptr = fopen(Buffer_Path, "wb");
 						if (wptr == NULL) {
 							printf("Couldn't open the file, error is: %s\n", strerror(errno));
 						}
 						fwrite(file_data[curr_client_ID], sizeof(U_CHAR), sizeof_file_dec[curr_client_ID], wptr);
+						fclose(wptr);
 						free(file_data[curr_client_ID]);
 						last_curr_recv_seq_ID[curr_client_ID] = 0;
+						printf("File received!\n");
+						already_came = false;
 					}
-
 				}else if (!already_came) {
-					memcpy(&file_data[idx[curr_client_ID] * DATA_LEN], Buffer_Rec, curr_recv_seq_ID_idx);
+					unsigned int idx = DATA_LEN * (curr_recv_seq_ID - 2);
+					memcpy(&file_data[curr_client_ID][idx], Buffer_Rec, curr_recv_seq_ID_idx);
+					already_came = true;
 				}
-				already_came = true;
 			}
 		}
 		FD_ZERO(&rset);
@@ -248,6 +267,7 @@ int main() {
 	
 	// Close the socket
 	closesocket(server_sock.data);
+	closesocket(server_sock.ack);
 	WSACleanup();
 	return 0;
 }
